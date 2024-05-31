a0600acd254a11b44950d3184ff531886d30db02
Muhamadfaruqosama <abdolslamet@gmail.com>
2024-05-31 07:23:24 +0700

    week 5 learning python

--- python/Week 3/try/trying_pandas_4.py
+++ python/Week 3/try/trying_pandas_4.py
@@ -5,7 +5,8 @@ store_data = pd.DataFrame({
     'location': ['Chicago', 'Boston', 'Seattle', 'San Francisco', 'Austin'],
     'gender': ['M','M','F','M','F'],
     'type': ['Electronics','Food&Beverages','Food&Beverages','Medicine','Beauty'],
-    'quantity':[1,3,4,2,1],'total_bill':[100,75,125,50,80]
+    'quantity':[1,3,4,2,1],
+    'total_bill':[100,75,125,50,80]
 })
 
 print(store_data)
